9a4f28b2d5d8984fcf0cc46285fe9592a2127b11
tacamula <dxbxdxbxdxb+dev@gmail.com>
2018-04-27 13:48:29 +0900

    until ex 1.6

---
+++ ss1/1.1.7.scm
@@ -0,0 +1,26 @@
+#|
+| mathmatics | programming |
+|------------|-------------|
+| describing properties of things | describ- ing how to do things |
+| declarative knowledg | imperative knowledge |
+| what is | how to |
+|#
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (print guess " ")
+  (print x " ") 
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
---
+++ ss1/ex1.3.scm
@@ -0,0 +1,8 @@
+(define (square x) (* x x))
+
+(define
+  (sum-of-square-mod a b c)
+    (cond
+      ((and (< a b) (< a c) (+ (square b) (square c))))
+      ((and (< b a) (< b c) (+ (square a) (square c))))
+      (else (+ (square a) (square b)))))
