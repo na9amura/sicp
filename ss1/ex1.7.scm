b86b1f18c2713fd96f6a26fd268cfcaccb9e9335
中村隆宏 <tacamula@nakamuratakahironoMacBook-puro.local>
2018-04-29 14:07:09 +0900

    update to check change degree instead of change volume

--- ss1/ex1.7.scm
+++ ss1/ex1.7.scm
@@ -9,9 +9,17 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-; 変化量が 0.001 未満ならOK
+; 変化量のが 0.001 未満ならOK
 (define (good-enough? prev guess)
-  (< (abs (- guess prev)) 0.001))
+  (write
+    (format
+      "prev: ~a, guess: ~a, diff: ~a, degree: ~a" 
+      prev
+      guess
+      (~r (- guess prev) #:precision 10)
+      (~r (/ (abs (- guess prev)) guess) #:precision 10)))
+  (newline)
+  (< (/ (abs (- guess prev)) guess) 0.0001))
 
 (define (sqrt x)
-  (sqrt-iter 1.1 1.0 x))
+  (sqrt-iter 10.0 1.0 x))
