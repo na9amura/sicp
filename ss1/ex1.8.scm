61af4b3b864031f0fd4cc6e898e28e6d09b26403
na9amura <dxbxdxbxdxb+dev@gmail.com>
2018-04-30 10:34:14 +0900

    finish implement cube's root

--- ss1/ex1.8.scm
+++ ss1/ex1.8.scm
@@ -1,3 +1,7 @@
+(define (square x) (* x x))
+
+(define (cube x) (* x x x))
+
 (define (improve-cube guess x)
   (/ 
     (+ 
@@ -5,16 +9,20 @@
       (* 2 guess))
     3))
 
-
-(define (cube-root-iter guess x)
-  (display guess) (newline) (display x) (newline)
-  (if good-enough-cube? guess x)
+(define (cube-root-iter prev guess x)
+  (if (good-enough-cube? prev guess)
     guess
-    (cube-root-iter (improve-cube guess x) x))
+    (cube-root-iter guess (improve-cube guess x) x)))
 
-(define (good-enough-cube? guess x)
-  (< (abs (- (cube guess) x)) 0.001))
+(define (good-enough-cube? prev guess)
+  (write
+    (format
+      "prev: ~a, guess: ~a, diff: ~a, degree: ~a" 
+      prev
+      guess
+      (~r (- guess prev) #:precision 10)
+      (~r (/ (abs (- prev guess)) guess) #:precision 10)))
+  (newline)
+  (< (/ (abs (- prev guess)) guess) 0.0001))
 
-(define (cube x) (* x x x))
-
-(define (cube-root x) (cube-root-iter 1.0 x))
+(define (cube-root x) (cube-root-iter 10.0 1.0 x))
