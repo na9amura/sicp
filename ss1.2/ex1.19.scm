0110c42506e431184e217f3578e49d5bdf648972
na9amura <dxbxdxbxdxb+dev@gmail.com>
2018-06-07 20:31:50 +0900

    forgot that apply T twice, and it is easier by thinking with variable not actual value
    http://www.billthelizard.com/2010/01/sicp-exercise-119-computing-fibonacci.html

--- ss1.2/ex1.19.scm
+++ ss1.2/ex1.19.scm
@@ -1,3 +1,7 @@
+
+### implement p' and q` in this procedure
+
+```s
 (define (fib n) 
   (fib-iter 1 0 0 1 n))
 
@@ -14,79 +18,91 @@
                     p
                     q
                     (- count 1)))))
+```
 
-(#|
+### asumption
+
+```
 T = { 
   a <- a + b
   b <- a  
 }
 
 T ^ n = [Fib(n + 1), Fib(n)]
+```
 
+when apply T **twice**, a and b transform like this
+
+```
 {
-  a <- bq + aq + ap
-  b <- bp + aq
-}
-
-- think calculate step by step, do not distinct counter is even or odd
-(fib-iter 1 0 0 1 5)
-                      (fib-iter (+ (* 0 1) (* 1 1) (* 1 0)) (+ (* 0 0) (* 1 1)) 0 1 (- 5 1))
-                      (fib-iter (+ 0 1 0) (+ 0 1) 0 1 4)
-(fib-iter 1 1 0 1 4)
-                      (fib-iter (+ (* 1 1) (* 1 1) (* 1 0)) (+ (* 1 0) (* 1 1)) 0 1 (- 4 1))
-                      (fib-iter (+ 1 1 0) (+ 0 1) 0 1 3)
-(fib-iter 2 1 0 1 3)
-                      (fib-iter (+ (* 1 1) (* 2 1) (* 2 0)) (+ (* 1 0) (* 2 1)) 0 1 (- 3 1))
-                      (fib-iter (+ 1 2 0) (+ 0 2) 0 1 2)
-(fib-iter 3 2 0 1 2)
-                      (fib-iter (+ (* 2 1) (* 3 1) (* 3 0)) (+ (* 2 0) (* 3 1)) 0 1 (- 2 1))
-                      (fib-iter (+ 2 3 0) (+ 0 3) 0 1 1)
-(fib-iter 5 3 0 1 1)
-                      (fib-iter (+ (* 3 1) (* 5 1) (* 5 0)) (+ (* 3 0) (* 5 1)) 0 1 (- 1 1))
-                      (fib-iter (+ 3 5 0) (+ 0 5) 0 1 0)
-(fib-iter 8 5 0 1 0)
-
-
-- if this procedure has implemented, arguments change like this.
-(fib-iter  1  0  0  1 7)
-(fib-iter  1  1  0  1 6)
-(fib-iter  1  1 p1 q1 3)
-(fib-iter  5  3 p1 q1 2)
-(fib-iter  5  3 p2 q2 1)
-(fib-iter 21 13 p2 q2 0)
-
-- for p1, q1
-{
-  1 * q1 + 1 * q1 + 1 * p1 = 5 
-  1 * q1 + 1 * p1 = 3
+  a' = bq + aq + ap
+  b' = bp + aq
 }
 
 {
-  2q1 + p1 = 5
-  p1 = 3 - q1
+  a'' = b'q + a'q + a'p
+  b'' = b'p + a'q
 }
+```
 
-2q1 + 3 - q1 = 5
-q1 = 2
-p1 = 1 
+rewrite equation by filling a' and b'
 
-- for p2, q2
-{
-  3 * q2 + 5 * q2 + 5 * p2 = 21
-  3 * q2 + 5 * p2 = 13
-}
+```
+b'' = (bp + aq) * p + (bq + aq + ap) * q
+    = bpp + apq + bqq + aqq + apq
+    = a(pq + qq + pq) + b(pp + qq)
+    = a(2pq + qq) + b(pp + qq)
+```
 
-{
-  8q2 + 5p2 = 21
-  p2 = (13 - 3q2) / 5
-}
+to fill `b'' = bp' + aq'`
 
-8q2 + 13 - 3q2 = 21
-5q2 = 8
-q2 = 8 / 5
-p2 = (13 - 3 * (8 / 5)) / 5
-   = (13 - 24 / 5) / 5
-   = ((65 - 24) / 5) / 5
-   = 41 / 5 * 1 / 5
-   = 41 / 25
-#|)
+```
+p' = p ^ 2 + q ^ 2
+q' = 2pq + p ^ 2
+```
+
+```
+a'' = (bp + aq) * q + (bq + aq + ap) * q + (bq + aq + ap) * p
+    = bpq + aqq + bqq + aqq + apq + bpq + apq + app
+    = bpq + bqq + bpq + aqq + aqq + apq + apq + app
+```
+
+as form of p' and q' already revealed,
+
+```
+    = 2bpq + bqq + (app + aqq) + (aqq + apq + apq)
+    = b(2pq + qq) + a(pp + qq) + a(2pq + qq)
+```
+
+verified p' and q' is the same form as resolved from b''
+
+then, p' and q' in scheme,
+
+```s
+; p'
+(+ (* p p) (* q q))
+
+; q'
+(+ (* 2 p q) (* q q))
+```
+
+complete definition
+
+```s
+(define (fib n) 
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond 
+    ((= count 0) b)
+    ((even? count) (fib-iter a 
+                             b
+                             (+ (* p p) (* q q)) 
+                             (+ (* 2 p q) (* q q))
+                             (/ count 2)))
+    (else (fib-iter (+ (* b q) (* a q) (* a p))
+                    (+ (* b p) (* a q))
+                    p
+                    q
+                    (- count 1)))))
+```
